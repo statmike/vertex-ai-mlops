4f44059393a1756cc58c27235455037ae6226998
Mike Henderson <statmike@gmail.com>
2023-02-03 12:44:40 +0000

    update tracking adding and removing

--- ga4_remove.py
+++ ga4_remove.py
@@ -5,23 +5,46 @@ import urllib.parse
 for root, dirs, files in os.walk('.'):
     for file in files:
         if file.endswith(('.md', '.ipynb')) and not root.endswith('.ipynb_checkpoints'):
-            print(os.path.join(root, file))
+            print('evaluating: ', os.path.join(root, file))
             
             if file.endswith('.md'):
+                update = False
+                
                 # read file contents
                 with open(os.path.join(root, file), 'r') as reader:
                     content = reader.readlines()
+                
+                # check for ga4
                 if content[0].startswith('![ga4](https://www.google-analytics.com'):
-                    print('update: ', file)
-                    #write back here
+                    content = content[1:]
+                    update = True
+                    
+                # update file if ga4 present
+                if update:
+                    print('make change here: ', file)
+                    with open(os.path.join(root, file), 'w') as writer:
+                        writer.writelines(content)
                     
             elif file.endswith('.ipynb'):
+                update = False
+                
                 # read file contents
                 with open(os.path.join(root, file), 'r') as reader:
                     content = json.loads(reader.read())
                     
+                # check for ga4
                 for cell in content['cells']:
                     if cell['cell_type'] == 'markdown':
                         if cell['source'][0].startswith('![ga4](https://www.google-analytics.com'):
-                            print('update: ', file)
+                            cell['source'] = cell['source'][1:]
+                            update = True
+                        # only review the first markdown cell the break for loop
+                        break
                 
+                # update file if ga4 present
+                if update:
+                    print('make change here: ', file)
+                    with open(os.path.join(root, file), 'w') as writer:
+                        writer.write(json.dumps(content))
+                    
+            print('done with evaluation.')
