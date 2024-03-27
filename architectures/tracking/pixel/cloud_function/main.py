33f98d224e1de612724ee43d4388aca650fe1ff5
statmike <statmike@gmail.com>
2024-03-27 00:38:19 +0000

    add client to data capture

--- architectures/tracking/pixel/cloud_function/main.py
+++ architectures/tracking/pixel/cloud_function/main.py
@@ -10,13 +10,15 @@ def tracking_pixel(request: flask.Request) -> flask.Response:
     
     repo_path = request.args.get('path', 'direct')
     repo_file = request.args.get('file', 'direct')
+    application = request.headers.get('User-Agent')
     
     if repo_path.startswith('statmike') and len(repo_path) < 500:
         if len(repo_file) > 5 and len(repo_file) < 500:
             data = dict(
                 event_timestamp = datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d %H:%M:%S.%f %Z"),
                 file_path = repo_path,
-                file_name = repo_file
+                file_name = repo_file,
+                client = application
             )
             message = json.dumps(data).encode('utf-8')
             future = pubsub_pubclient.publish(
