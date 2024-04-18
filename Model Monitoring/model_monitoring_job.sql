c0166fc3b3a20aea50a84ad89655f654dcb658cf
statmike <statmike@gmail.com>
2024-04-18 14:15:23 +0000

    updated scheduled query to only alert when anomalies detected

--- Model Monitoring/model_monitoring_job.sql
+++ Model Monitoring/model_monitoring_job.sql
@@ -27,11 +27,10 @@ SET drift_anomalies = (
     )
     WHERE is_anomaly = True
 );
-IF(ARRAY_LENGTH(drift_anomalies) > 0)
-    THEN
-        SET drift = CONCAT(
-            "Found data drift (", ARRAY_TO_STRING(drift_anomalies, ", "), ")"
-        );
+IF(ARRAY_LENGTH(drift_anomalies) > 0) THEN
+    SET drift = CONCAT(
+        "Found data drift (", ARRAY_TO_STRING(drift_anomalies, ", "), ")"
+    );
     ELSE SET drift = 'No drift detected.';
 END IF;
 
@@ -48,110 +47,100 @@ SET skew_anomalies = (
     )
     WHERE is_anomaly = True
 );
-IF(ARRAY_LENGTH(skew_anomalies) > 0)
-    THEN
-        SET skew = CONCAT(
-            "Found data skew (", ARRAY_TO_STRING(skew_anomalies, ", "), ")"
-        );
+IF(ARRAY_LENGTH(skew_anomalies) > 0) THEN
+    SET skew = CONCAT(
+        "Found data skew (", ARRAY_TO_STRING(skew_anomalies, ", "), ")"
+    );
     ELSE SET skew = 'No skew detected.';
 END IF;
 
 # Prepare Alert
-IF(ARRAY_LENGTH(drift_anomalies) > 0 OR ARRAY_LENGTH(skew_anomalies) > 0)
-    THEN
-        BEGIN
-            DECLARE train_accuracy FLOAT64;
-            DECLARE recent_accuracy FLOAT64;
-            DECLARE retrain_accuracy FLOAT64;
-            
-            # get current models evaluation metrics: accuracy
-            SET train_accuracy = (
-                SELECT accuracy
-                FROM ML.EVALUATE (
-                    MODEL `statmike-mlops-349915.bqml_model_monitoring.classify_species_rf`
-                )
-            );
-            
-            # get current models recent evaluation metrics: accuracy
-            SET recent_accuracy = (
-                SELECT accuracy
-                FROM ML.EVALUATE (
-                    MODEL `statmike-mlops-349915.bqml_model_monitoring.classify_species_rf`,
-                    (
-                        SELECT *
-                        FROM `statmike-mlops-349915.bqml_model_monitoring.serving`
-                        WHERE DATE(instance_timestamp) >= DATE_SUB(@run_date, INTERVAL 1 WEEK) 
-                    )
-                )
-            );
-            
-            # retrain the model
-            CREATE OR REPLACE MODEL `statmike-mlops-349915.bqml_model_monitoring.classify_species_rf`
-                TRANSFORM(
-                    ML.ROBUST_SCALER(body_mass_g) OVER() AS body_mass_g,
-                    ML.STANDARD_SCALER(culmen_length_mm) OVER() AS culmen_length_mm,
-                    ML.STANDARD_SCALER(culmen_depth_mm) OVER() AS culmen_depth_mm,
-                    ML.QUANTILE_BUCKETIZE(flipper_length_mm, 3) OVER() AS flipper_length_mm,
-                    ML.IMPUTER(sex, 'most_frequent') OVER() AS sex,
-                    ML.IMPUTER(island, 'most_frequent') OVER() AS island,
-                    species
-                )
-                OPTIONS(
-                    MODEL_TYPE = 'RANDOM_FOREST_CLASSIFIER',
-                    INPUT_LABEL_COLS = ['species'],
+IF(ARRAY_LENGTH(drift_anomalies) > 0 OR ARRAY_LENGTH(skew_anomalies) > 0) THEN
+    BEGIN
+        DECLARE train_accuracy FLOAT64;
+        DECLARE recent_accuracy FLOAT64;
+        DECLARE retrain_accuracy FLOAT64;
 
-                    # data specifics
-                    DATA_SPLIT_METHOD = 'AUTO_SPLIT',
+        # get current models evaluation metrics: accuracy
+        SET train_accuracy = (
+            SELECT accuracy
+            FROM ML.EVALUATE (
+                MODEL `statmike-mlops-349915.bqml_model_monitoring.classify_species_rf`
+            )
+        );
 
-                    # model specifics
-                    AUTO_CLASS_WEIGHTS = FALSE,
-                    NUM_PARALLEL_TREE= 150,
-                    TREE_METHOD = 'HIST',
-                    SUBSAMPLE = 0.85,
-                    COLSAMPLE_BYTREE = 0.9,
-                    ENABLE_GLOBAL_EXPLAIN = TRUE,
+        # get current models recent evaluation metrics: accuracy
+        SET recent_accuracy = (
+            SELECT accuracy
+            FROM ML.EVALUATE (
+                MODEL `statmike-mlops-349915.bqml_model_monitoring.classify_species_rf`,
+                (
+                    SELECT *
+                    FROM `statmike-mlops-349915.bqml_model_monitoring.serving`
+                    WHERE DATE(instance_timestamp) >= DATE_SUB(@run_date, INTERVAL 1 WEEK) 
+                )
+            )
+        );
 
-                    # register model in Vertex AI For Online Serving
-                    MODEL_REGISTRY = 'VERTEX_AI'
-                )
-            AS
-                SELECT species, island, culmen_length_mm, culmen_depth_mm, sex, flipper_length_mm, body_mass_g
-                FROM `statmike-mlops-349915.bqml_model_monitoring.training_split`
-                WHERE splits = 'TRAIN'
-                UNION ALL
-                SELECT species, island, culmen_length_mm, culmen_depth_mm, sex, flipper_length_mm, body_mass_g
-                FROM `statmike-mlops-349915.bqml_model_monitoring.serving`
-                WHERE DATE(instance_timestamp) < @run_date
-            ;
-            
-            # get retrained models evaluation metrics: accuracy
-            SET retrain_accuracy = (
-                SELECT accuracy
-                FROM ML.EVALUATE (
-                    MODEL `statmike-mlops-349915.bqml_model_monitoring.classify_species_rf`
-                )
-            );
+        # retrain the model
+        CREATE OR REPLACE MODEL `statmike-mlops-349915.bqml_model_monitoring.classify_species_rf`
+            TRANSFORM(
+                ML.ROBUST_SCALER(body_mass_g) OVER() AS body_mass_g,
+                ML.STANDARD_SCALER(culmen_length_mm) OVER() AS culmen_length_mm,
+                ML.STANDARD_SCALER(culmen_depth_mm) OVER() AS culmen_depth_mm,
+                ML.QUANTILE_BUCKETIZE(flipper_length_mm, 3) OVER() AS flipper_length_mm,
+                ML.IMPUTER(sex, 'most_frequent') OVER() AS sex,
+                ML.IMPUTER(island, 'most_frequent') OVER() AS island,
+                species
+            )
+            OPTIONS(
+                MODEL_TYPE = 'RANDOM_FOREST_CLASSIFIER',
+                INPUT_LABEL_COLS = ['species'],
 
-            SELECT ERROR(
-                CONCAT(
-                    "\n\nMonitoring Report for ", @run_date, ":",
-                    "\n\t", drift,
-                    "\n\t", skew,
-                    "\nThe Model was retrained:",
-                    "\n\taccuracy of prior model: ", train_accuracy,
-                    "\n\trecent accuracy of prior model: ", recent_accuracy,
-                    "\n\taccuracy after retraining: ", retrain_accuracy,
-                    "\n\n"
-                )
-            );
-        END;
-    ELSE
+                # data specifics
+                DATA_SPLIT_METHOD = 'AUTO_SPLIT',
+
+                # model specifics
+                AUTO_CLASS_WEIGHTS = FALSE,
+                NUM_PARALLEL_TREE= 150,
+                TREE_METHOD = 'HIST',
+                SUBSAMPLE = 0.85,
+                COLSAMPLE_BYTREE = 0.9,
+                ENABLE_GLOBAL_EXPLAIN = TRUE,
+
+                # register model in Vertex AI For Online Serving
+                MODEL_REGISTRY = 'VERTEX_AI'
+            )
+        AS
+            SELECT species, island, culmen_length_mm, culmen_depth_mm, sex, flipper_length_mm, body_mass_g
+            FROM `statmike-mlops-349915.bqml_model_monitoring.training_split`
+            WHERE splits = 'TRAIN'
+            UNION ALL
+            SELECT species, island, culmen_length_mm, culmen_depth_mm, sex, flipper_length_mm, body_mass_g
+            FROM `statmike-mlops-349915.bqml_model_monitoring.serving`
+            WHERE DATE(instance_timestamp) < @run_date
+        ;
+
+        # get retrained models evaluation metrics: accuracy
+        SET retrain_accuracy = (
+            SELECT accuracy
+            FROM ML.EVALUATE (
+                MODEL `statmike-mlops-349915.bqml_model_monitoring.classify_species_rf`
+            )
+        );
+
         SELECT ERROR(
             CONCAT(
                 "\n\nMonitoring Report for ", @run_date, ":",
                 "\n\t", drift,
                 "\n\t", skew,
+                "\nThe Model was retrained:",
+                "\n\taccuracy of prior model: ", train_accuracy,
+                "\n\trecent accuracy of prior model: ", recent_accuracy,
+                "\n\taccuracy after retraining: ", retrain_accuracy,
                 "\n\n"
             )
         );
+    END;
+    # ELSE ... supressed alerts on runs without anomalies detected
 END IF;
