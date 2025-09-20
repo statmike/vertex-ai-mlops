b8885fe0f435613e24bfd8f88f3a0bfcc5d780af
Mike Henderson <statmike@gmail.com>
2025-09-20 15:05:42 +0000

    updates

--- MLOps/Feature Store/test.py
+++ MLOps/Feature Store/test.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 
-def get_current_time_string() -> str:
+def get_current_time() -> str:
   """
   Returns a string with the current date and time.
   """
   now = datetime.now()
   formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
-  return f"Hey!, the time is current {formatted_time}"
+  return f"Hey, the time is currently {formatted_time}."
 
 # Example usage:
-#print(get_current_time_string())
+#print(get_current_time())
