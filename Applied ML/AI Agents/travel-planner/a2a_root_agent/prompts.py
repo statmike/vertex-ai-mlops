3a8238b1360d76c2e8fbfd4195476c67318337ba
karticn-google <karticn@google.com>
2025-10-15 09:47:35 -0500

    changes to table names and prompts.

--- Applied ML/AI Agents/travel-planner/a2a_root_agent/prompts.py
+++ Applied ML/AI Agents/travel-planner/a2a_root_agent/prompts.py
@@ -1,26 +1,19 @@
-project_id = "your-project-id"
-
 attractions_planner_prompt = """
         - Provide the user options for attractions to visit within their selected country.
-        - When they reply, use your tool to save their selected attraction using the save attractions to the state tool
-        and then provide more possible attractions.
-        - If the user asks to display the places they have visited in the past, use the bq_toolbox tool to retrieve the places and display them.
-        - If they ask to view the list, provide a bulleted list of
-        {{ attractions? }} and then suggest some more.
         """
 
-places_of_interest_prompt = f"""
+places_of_interest_prompt = """
         Perform the following steps when the user picks an attraction in a country of interest:
         **Step 1: Get Table Metadata**
         - Your first action **MUST** be to use the `bigquery_get_table_info` tool to get the schema for the `attractions` table in the 'bq_adk_ds' dataset. This is essential for understanding the available columns.
         **Step 2: Write and Execute SQL**
-        - After you have the table schema, use the user's original question and the column information to write a precise BigQuery SQL query referencing the `{project_id}.bq_adk_ds.attractions` table only.
+        - After you have the table schema, use the user's original question and the column information to write a precise BigQuery SQL query referencing the `vertexai-demo-ltfpzhaw.bq_adk_ds.attractions` table only.
         - Once you have written the query, you **MUST** use the `execute_sql_tool` to run it. Confirm to the user that the attraction has been added to the list.
         Perform the following steps when the user requests to list the attractions they have picked in the country of interest:
         **Step 1: Get Table Metadata**
         - Your first action **MUST** be to use the `bigquery_get_table_info` tool to get the schema for the `attractions` table in the 'bq_adk_ds' dataset. This is essential for understanding the available columns.
         **Step 2: Write and Execute SQL**
-        - After you have the table schema, use the user's original question and the column information to write a precise BigQuery SQL query referencing the `{project_id}.bq_adk_ds.attractions` table only.
+        - After you have the table schema, use the user's original question and the column information to write a precise BigQuery SQL query referencing the `vertexai-demo-ltfpzhaw.bq_adk_ds.attractions` table only.
         - Once you have written the query, you **MUST** use the `execute_sql_tool` to run it. Display the list of attractions picked by the user in the country of interest.
         """
 
--- Applied ML/AI Agents/travel-planner/a2a_root_agent/remote_a2a/attractions_planner_agent/agent.py
+++ Applied ML/AI Agents/travel-planner/a2a_root_agent/remote_a2a/attractions_planner_agent/agent.py
@@ -11,21 +11,25 @@ from a2a_root_agent.prompts import attractions_planner_prompt
 load_dotenv()
 model_name = os.getenv("MODEL")
 
-def save_attractions_to_state(
-    tool_context: ToolContext,
-    attractions: List[str]
-) -> dict[str, str]:
-    """Saves the list of attractions to state["attractions"].
 
-    Args:
-        attractions [str]: a list of strings to add to the list of attractions
+# Tools (add the tool here when instructed)
 
-    Returns:
-        None
-    """
-    existing_attractions = tool_context.state.get("attractions", [])
-    tool_context.state["attractions"] = existing_attractions + attractions
-    return {"status": "success"}
+
+# def save_attractions_to_state(
+#     tool_context: ToolContext,
+#     attractions: List[str]
+# ) -> dict[str, str]:
+#     """Saves the list of attractions to state["attractions"].
+
+#     Args:
+#         attractions [str]: a list of strings to add to the list of attractions
+
+#     Returns:
+#         None
+#     """
+#     existing_attractions = tool_context.state.get("attractions", [])
+#     tool_context.state["attractions"] = existing_attractions + attractions
+#     return {"status": "success"}
 
 
 # Agents
@@ -37,7 +41,6 @@ attractions_planner = Agent(
     instruction=attractions_planner_prompt,
     before_model_callback=log_query_to_model,
     after_model_callback=log_model_response,
-    tools=[save_attractions_to_state]
     )
 
 a2a_app2 = to_a2a(attractions_planner, port=8002)
